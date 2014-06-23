0f00717a0f6eb1faedd0dffee2544b089917f598
innova <innova@louis-pc>
2014-06-23 10:31:08 +0200

    Nettoyage

--- php/includes/head.php
+++
@@ -1,8 +0,0 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-
-<!-- CSS -->
-<link rel="stylesheet" href="../../css/style.css">
